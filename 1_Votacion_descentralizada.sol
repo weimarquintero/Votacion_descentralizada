2d1252c7c8aa5e7b8a210bd06a3209d654b399fe
weimarquintero <weimar.quintero@udea.edu.co>
2024-09-26 16:52:40 -0500

    Creación de variables del contrato

--- 1_Votacion_descentralizada.sol
+++ 1_Votacion_descentralizada.sol
@@ -3,5 +3,19 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 /**
- * @title Votacion Descentralizada
+ * @title Votacion (Descentralizada)
  */
+
+ contract Votacion {
+
+    struct Propuesta {
+        string nombre;
+        uint8 votosTotales;
+    }
+
+    address public propietario;
+    mapping(address => bool) public whiteList;
+    mapping(address => bool) public sufragantes;
+    Propuesta[] public propuestas;
+    uint8 public tiempoVotacion;
+ }
